b6a29580bd14e55d16006564ce50bd9504d82be5
Ajit Chakrapani <absessive@gmail.com>
2013-02-12 21:06:54 -0500

    graph in Currencies
    added graph in currencies

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -10,6 +10,6 @@ class Visit < ActiveRecord::Base
 	
 	# Given a date returns total currencies collected till that date for the user
 	def self.total_collected(date, user)
-		
+		Visit.find_by_sql("select * from visits, currencies where (visits.country_code  = currencies.country_id) and visits.username = '#{user}' and (DATE(visits.created_at) )<= '#{date}'").count
 	end
 end
