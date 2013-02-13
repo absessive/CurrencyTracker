9b1f8bc24aacdb7366ebc1fedc7341354215eddb
Ajit Chakrapani <absessive@gmail.com>
2013-02-12 20:54:14 -0500

    Graph - currency
    Added graph in currencies page

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,14 @@ class Visit < ActiveRecord::Base
 	def is_visited?(user, country)
 		username = Visit.find_by_username(user).id
 	end
+	
+	# Given a date returns total countries visited till that date for the user
+	def self.total_visited(date, user)
+		Visit.where("DATE(created_at) <= ? and username='#{user}' ", date).count
+	end
+	
+	# Given a date returns total currencies collected till that date for the user
+	def self.total_collected(date, user)
+		
+	end
 end
